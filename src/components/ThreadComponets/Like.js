afe5601b9687fecf33656fe4e8b1ed88041eade3
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-09-17 10:57:05 -0500

    Updates to Like

--- src/components/ThreadComponets/Like.js
+++ src/components/ThreadComponets/Like.js
@@ -6,39 +6,40 @@ import { useDispatch, useSelector } from 'react-redux'
 import { likeThread } from '../../features/threads/threadSlice'
 
 const Like = ({ threadId }) => {
-  const location = useLocation()
   const [liked, setLiked] = useState(false)
   const { user } = useSelector((state) => state.auth)
   const { allThreads, threads } = useSelector((state) => state.threads)
 
   const parsedUser = JSON.parse(localStorage.getItem('user'))
 
+  const location = useLocation()
   const dispatch = useDispatch()
 
   useEffect(() => {
+    let localThreads
+
     switch (location.pathname) {
       case '/':
-        console.log(threadId)
-        const allThread = allThreads.find((thread) => thread._id === threadId)
-        setLiked(
-          allThread?.likedBy?.includes(user.id || parsedUser.id) || false
-        )
+        localThreads = allThreads.find((thread) => thread._id === threadId)
         break
       case '/profile':
-        console.log(threadId)
-        const profileThread = threads.find((thread) => thread._id === threadId)
-        setLiked(
-          profileThread?.likedBy?.includes(user.id || parsedUser.id) || false
-        )
+        localThreads = threads.find((thread) => thread._id === threadId)
         break
       default:
         break
     }
+
+    setLiked(localThreads?.likedBy?.includes(user.id || parsedUser.id) || false)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname, parsedUser.id, threadId, user.id])
 
   const handleLike = async () => {
-    dispatch(likeThread(threadId))
+    if (liked === false) {
+      dispatch(likeThread(threadId))
+      setLiked(true)
+    } else {
+      setLiked(false)
+    }
   }
 
   return (
